9adb97d1bb8ed6dc3daaf035358b868f4164e150
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-10-06 14:14:40 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,23 @@
 var elixir = require('laravel-elixir');
 
 elixir(function (mix) {
-    mix.copy(
-        'vendor/bower_components/jquery/dist/jquery.min.js',
-        'public/js/vendor/jquery.min.js'
-    );
-    mix.copy(
-        'vendor/bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'public/js/vendor/bootstrap.min.js'
-    );
-    mix.copy(
-        'vendor/bower_components/sweetalert/dist/sweetalert.min.js',
-        'public/js/vendor/sweetalert.min.js'
-    );
-
-    mix.copy(
-        'vendor/bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap/',
-        'resources/assets/sass/bootstrap/'
-    );
-    mix.copy(
-        'vendor/bower_components/bootstrap-sass-official/assets/stylesheets/_bootstrap.scss',
-        'resources/assets/sass/_bootstrap.scss'
-    );
-    mix.copy(
-        'vendor/bower_components/sweetalert/dist/sweetalert.css',
-        'public/css/vendor/sweetalert.css'
-    );
-
-    mix.copy(
-        'vendor/bower_components/fontawesome/fonts',
-        'public/fonts/font-awesome'
-    );
-
     mix.sass('app.scss', 'public/css/app');
+
     mix.styles([
-        'vendor/font-awesome.css',
-        'vendor/sweetalert.css'
-    ], 'public/css/vendor.css', 'public/css');
+        'bootstrap/dist/css/bootstrap.min.css',
+        'fontawesome/css/font-awesome.min.css',
+        'sweetalert/dist/sweetalert.css',
+    ], 'public/css/vendor.css', 'vendor/bower-components');
     mix.styles([
-        'app/app.css',
-        'vendor.css'
+        'vendor.css',
+        'app/app.css'
     ], null, 'public/css');
 
     mix.scripts([
-        'vendor/jquery.min.js',
-        'vendor/bootstrap.min.js',
-        'vendor/sweetalert.min.js'
-    ], 'public/js/vendor.js', 'public/js');
+        'jquery/dist/jquery.min.js',
+        'bootstrap/dist/js/bootstrap.min.js',
+        'sweetalert/dist/sweetalert.min.js',
+    ], 'public/js/vendor.js', 'vendor/bower-components');
     mix.scripts([
         'vendor.js',
         'app/app.js'
