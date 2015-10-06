83a26344bc70b079e487818f3e792476e5396907
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-10-06 09:20:35 +0700

    Use bower for assets management.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,56 @@
 var elixir = require('laravel-elixir');
 
-elixir(function(mix) {
+elixir(function (mix) {
+    mix.copy(
+        'vendor/bower_components/jquery/dist/jquery.min.js',
+        'public/js/vendor/jquery.min.js'
+    );
+    mix.copy(
+        'vendor/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'public/js/vendor/bootstrap.min.js'
+    );
+    mix.copy(
+        'vendor/bower_components/sweetalert/dist/sweetalert.min.js',
+        'public/js/vendor/sweetalert.min.js'
+    );
+
+    mix.copy(
+        'vendor/bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap/',
+        'resources/assets/sass/bootstrap/'
+    );
+    mix.copy(
+        'vendor/bower_components/bootstrap-sass-official/assets/stylesheets/_bootstrap.scss',
+        'resources/assets/sass/_bootstrap.scss'
+    );
+    mix.copy(
+        'vendor/bower_components/sweetalert/dist/sweetalert.css',
+        'public/css/vendor/sweetalert.css'
+    );
+
+    mix.copy(
+        'vendor/bower_components/fontawesome/fonts',
+        'public/fonts/font-awesome'
+    );
+
     mix.sass('app.scss', 'public/css/app');
+    mix.styles([
+        'vendor/font-awesome.css',
+        'vendor/sweetalert.css'
+    ], 'public/css/vendor.css', 'public/css');
+    mix.styles([
+        'app/app.css',
+        'vendor.css'
+    ], null, 'public/css');
 
-    mix.stylesIn('public/css/vendor', 'public/css/vendor.css');
-    mix.styles(['app/app.css', 'vendor.css'], null, 'public/css');
-
-    mix.scriptsIn('public/js/vendor', 'public/js/vendor.js');
-    mix.scripts(['vendor.js', 'app/app.js'], null, 'public/js');
+    mix.scripts([
+        'vendor/jquery.min.js',
+        'vendor/bootstrap.min.js',
+        'vendor/sweetalert.min.js'
+    ], 'public/js/vendor.js', 'public/js');
+    mix.scripts([
+        'vendor.js',
+        'app/app.js'
+    ], null, 'public/js');
 
     mix.version(['public/css/all.css', 'public/js/all.js']);
 });
