de113bef97882e2e2ca25ec6f56b537f8f8f619a
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-10-09 23:10:11 +0700

    Improve category view.

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -2,59 +2,78 @@
 @section('title', 'Categories')
 @section('content')
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-10 col-md-offset-1">
             @if(blank($categories))
                 <div class="text-center text-warning">No category available.</div>
             @else
                 <div class="admin-wrapper">
-                    <div class="panel panel-info">
-                        <div class="panel-heading"><strong>List of categories</strong></div>
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Description</th>
-                                    <th>Published</th>
-                                    <th>Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($categories->chunk(25) as $categoryList)
-                                    @foreach($categoryList as $category)
-                                        <tr>
-                                            <td>{{ $category->id }}</td>
-                                            <td>{{ $category->name }}</td>
-                                            <td>{{ $category->description }}</td>
-                                            <td>{{ short_time($category->created_at) }}</td>
-                                            <td>
-                                                <a href="{{ route('admin.categories.edit', $category) }}"
-                                                   class="btn btn-info btn-xs"
-                                                   data-toggle="tooltip"
-                                                   data-placement="bottom"
-                                                   title="Update this category">
-                                                    <i class="fa fa-pencil"></i>
-                                                </a>
-                                                @include('admin.categories.partials._delete_form')
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endforeach
-                                </tbody>
-                            </table>
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <strong class="text-info">{{ plural('CATEGORY', counting($categories)) }}</strong>
+
+                            <div class="pull-right">
+                                <button type="button"
+                                        class="btn btn-primary btn-xs"
+                                        data-toggle="modal"
+                                        data-target="#add-category">
+                                    <i class="fa fa-plus"></i>
+                                </button>
+                            </div>
                         </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Published</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($categories->chunk(25) as $categoryList)
+                                @foreach($categoryList as $category)
+                                    <tr>
+                                        <td>{{ $category->id }}</td>
+                                        <td>{{ $category->name }}</td>
+                                        <td>{{ $category->description }}</td>
+                                        <td>{{ short_time($category->created_at) }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.categories.edit', $category) }}"
+                                               class="btn btn-info btn-xs"
+                                               data-toggle="tooltip"
+                                               data-placement="bottom"
+                                               title="Update this category">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                            @include('admin.categories.partials._delete_form')
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 {!! paginate($categories) !!}
             @endif
         </div>
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <strong>Add New Category</strong>
+    </div>
+    <div class="modal fade" id="add-category" data-backdrop="static" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Add New Category</h4>
                 </div>
-                <div class="panel-body">
+                <div class="modal-body">
                     {!! Form::model($category = new \FELS\Entities\Category, ['route' => 'admin.categories.store']) !!}
                         @include('admin.categories.partials._main_form', ['categorySubmit' => 'Add'])
                     {!! Form::close() !!}
@@ -62,4 +81,13 @@
             </div>
         </div>
     </div>
+@stop
+@section('footer')
+    @if($errors->all())
+        <script>
+            $(function () {
+                $('#add-category').modal('show');
+            });
+        </script>
+    @endif
 @stop
